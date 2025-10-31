095a29f1c8c0b897df297bc0522cb4783bad8e38
Karthik89789 <Karakatlak@gmail.com>
2025-10-31 13:43:29 +0530

    Add files via upload

---
+++ Cognizantcapston/src/com/cognizant/kafka/Deserializer/AccountDeserializer.java
@@ -0,0 +1,22 @@
+package com.cognizant.kafka.Deserializer;
+
+import com.cognizant.kafka.domain.Account;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.common.serialization.Deserializer;
+
+import java.io.IOException;
+
+public class AccountDeserializer implements Deserializer<Account> {
+    private ObjectMapper mapper = new ObjectMapper();
+
+    @Override
+    public Account deserialize(String topic, byte[] bytes) {
+        Account account = null;
+        try {
+            account = mapper.readValue(bytes, Account.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return account;
+    }
+}
